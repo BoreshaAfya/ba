6738fc10bb5a2f0f25b738ca8cbdbccc425a91db
Faith Muli <faithmuli33@gmail.com>
2015-09-11 17:39:12 +0300

    initial commit

---
+++ app/src/main/java/program/com/ba/Splashscreen.java
@@ -0,0 +1,48 @@
+package program.com.ba;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Splashscreen extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(Splashscreen.this, MainActivity.class);
+                    Splashscreen.this.startActivity(mainIntent);
+                Splashscreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splashscreen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
